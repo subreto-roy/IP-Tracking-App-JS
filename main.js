34528495f735d0ea2a7d09839f90645e4e298087
subreto-roy <sroy172182@bscse.uiu.ac.bd>
2023-03-22 13:08:49 +0600

    ok

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ document.getElementById('addForm').addEventListener('submit', function(e) {
   if(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(ip)){
         axios.get('http://ip-api.com/json/' + ip)
         .then(function (response) {
-        var details = response.data;
-        console.log(response);
+            var details = response.data;
+            console.log(response);
         
         
-        document.getElementById('apiDetails').innerHTML = '<p>City: ' + details.city + '</p><p>Country: ' + details.country + '</p><p>ISP: ' + details.isp + '</p><p>Latitute: ' + details.lat + '</p><p>Longitude: ' + details.lon + '</p>';
+            // document.getElementById('apiDetails').innerHTML = '<p>City: ' + details.city + '</p><p>Country: ' + details.country + '</p><p>ISP: ' + details.isp + '</p><p>Latitute: ' + details.lat + '</p><p>Longitude: ' + details.lon + '</p>';
         })
         .catch(function (error) {
-        console.log(error);
+            console.log(error);
         });
     } else{
         alert('Invalid Ip address');
